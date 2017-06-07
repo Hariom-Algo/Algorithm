97d01970833770a197fd0a4485d5c767c7480cda
Singh <HSINGH@arinc.com>
2017-06-07 15:48:28 -0400

    python file updated

--- Graph/Adjacency_List/Python/Graph.py
+++ Graph/Adjacency_List/Python/Graph.py
@@ -5,13 +5,34 @@ Created on Tue May 23 11:44:02 2017
 @author: HSINGH
 """
 class Graph:
-    def __init__(self):
+    """
+      Read the Intialized Graph and Create a Adjacency list out of it 
+	   There could be cases where in the initialized graph <map> link
+      issues are not maintained
+	   for example node 2 to 1 link 
+	    2->1
+	   there needs to be a link then since undirected Graph
+	    1->2
+    """
+    
+    def __init__(self,Graph_init):
         self.edge={}
-        
+        for keys,values in Graph_init.items():
+             for value in values:
+                 self.addEdge(keys,value);
+      
+    """
+    Add a vertex to graph map
+	structure is
+	int => int list
+    """
     def addVertex(self,v):
         if v not in self.edge:
             self.edge[v]=[]
-            
+    """
+	Add Edge from both vertex to each other
+	Make sure the nodes are present   
+   """      
     def addEdge(self,u,v):
         if u not in self.edge:
             self.addVertex(u)
@@ -28,6 +49,28 @@ class Graph:
         if v not in self.edge:
             return False 
         return  u in self.edge[v] 
+    
+    def display(self):
+        for keys,values in self.edge.items():
+            print(keys,":=>",values)
+
+"""A initalized Graph (not in form of adjaceny list"""
+Graph_init = {1:[2,3,5],
+              2:[1,4],
+              3:[1,6]};
+              
+"""Default constrcutor takes care of making the initialzed map to adjaceny list"""                 
+g=Graph(Graph_init)
+g.addVertex(1)
+g.addVertex(2) 
+g.addVertex(3)
+g.addEdge(1,2)
+g.addEdge(3,2)
+g.display();
+         
+
+              
+              
             
             
             
